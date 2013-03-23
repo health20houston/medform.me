b063ce38129db113d29a81691561c1d24a6389cb
Antony Wu <antony@meshway.com>
2013-03-23 14:13:18 -0700

    EmergencyContact: check in missing check to People model

--- model/people.py
+++ model/people.py
@@ -17,7 +17,7 @@ class Person(Entity):
 
 	ssn = Field(Unicode(150), nullable=True)
 	phoneNumbers = OneToMany("PhoneNumber")
-	emergencies = OneToMany("Emergency")
+	emergencyContacts = OneToMany("EmergencyContact")
 
 	def __repr__(self):
 		return "<Person %s %s (%s)>" % (self.firstName, self.lastName, self.id)
