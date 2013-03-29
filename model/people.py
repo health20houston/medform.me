d0b47bbc8eac18ada995b4acee74068a70fced06
Jose Pablo Ramirez <jp.ramirezpra@gmail.com>
2013-03-29 17:42:35 -0500

    Deleted Log Model, and moved the classes over into the people.py to fix the EventLog target unresolved issue

--- model/people.py
+++ model/people.py
@@ -50,7 +50,7 @@ class Patient(Entity):
 
 ####Relationships to event Logging
 	eventLog = OneToMany("EventLog")
-	accessLog = OneToMany("accessLog")
+	accessLog = OneToMany("AccessLog")
 
 	def __repr__(self):
 		return "<Patient %s %s (%s)>" % (self.firstName, self.lastName, self.id)
@@ -100,3 +100,29 @@ class PatientCode(Entity):
 	@staticmethod
 	def validateCode(code):
 		return PatientCode.get_by(code=code)
+
+class EventLog(Entity):
+	using_options(tablename="eventLog") 
+
+	eventType = Field(Enum("info", "error"), nullable=False)
+	eventTimeStamp = Field(DateTime(), nullable=False)
+	errorMessage = Field(Unicode(100), nullable=False)
+	origFunction = Field(Unicode(25), nullable=True)
+	userAssoc = ManyToOne("Patient")
+
+	def __repr__(self):
+		return "<Access Log Time: %s, Type: %s, ErrorMessage: (%s)>" % (self.eventTimeStamp, self.eventType, self.errorMessage)
+
+class AccessLog(Entity):
+	using_options(tablename="accessLog")
+
+	code = Field(Unicode(5), nullable=False)
+	ipAddress = Field(Unicode(45), nullable=False) #taking into account IPv6
+	expirationDateTime = Field(DateTime(), nullable=False)
+	accessTime = Field(DateTime(), nullable=False)
+	accessingPractice = Field(Unicode(50), nullable=False)
+	accessingPerson = Field(Unicode(50), nullable=False)
+	patient = ManyToOne("Patient")
+
+	def __repr__(self):
+		return "<Accessed By: %s, @: %s, for: %s >" % (self.accessingPerson, self.accessTime, self.patient)
